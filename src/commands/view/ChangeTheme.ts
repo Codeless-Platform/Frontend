9a8c7e81c29320bffced4ecdd78eb32025aa13d3
Kamilia98 <kamiliaahmed01@gmail.com>
2024-06-12 15:23:26 +0300

    Improved functionality of change theme button

--- src/commands/view/ChangeTheme.ts
+++ src/commands/view/ChangeTheme.ts
@@ -7,11 +7,15 @@ export default {
     rootElement.style.setProperty('--gjs-primary-color', 'white');
     rootElement.style.setProperty('--gjs-main-color', 'white');
     rootElement.style.setProperty('--gjs-secondary-color', 'black');
-    console.log(this);
+    rootElement.style.setProperty('--gjs-font-color', 'black');
+    rootElement.style.setProperty('--gjs-codeMirror-color', 'white');
   },
   stop(ed) {
     const rootElement = document.documentElement;
     rootElement.style.setProperty('--gjs-primary-color', '#444');
+    rootElement.style.setProperty('--gjs-main-color', '#444');
     rootElement.style.setProperty('--gjs-secondary-color', '#ddd');
+    rootElement.style.setProperty('--gjs-font-color', '#ddd');
+    rootElement.style.setProperty('--gjs-codeMirror-color', '#322931');
   },
 } as CommandObject;
